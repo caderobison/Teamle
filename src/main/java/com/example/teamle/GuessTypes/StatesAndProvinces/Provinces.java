904657d653b91a209292ac5402a17321622d190d
caderobison <cade.robison@gmail.com>
2025-01-07 00:06:03 -0600

    oh canada

--- src/main/java/com/example/teamle/GuessTypes/StatesAndProvinces/Provinces.java
+++ src/main/java/com/example/teamle/GuessTypes/StatesAndProvinces/Provinces.java
@@ -1,4 +1,17 @@
 package com.example.teamle.GuessTypes.StatesAndProvinces;
 
 public enum Provinces implements StatesAndProvinces{
+    ALBERTA,
+    BRITISH_COLUMBIA,
+    MANITOBA,
+    NEW_BRUNSWICK,
+    NEWFOUNDLAND_LABRADOR,
+    NOVA_SCOTIA,
+    ONTARIO,
+    PRINCE_EDWARD_ISLAND,
+    QUEBEC,
+    SASKATCHEWAN,
+    NORTHWEST_TERRITORIES,
+    NUNAVUT,
+    YUKON,
 }
