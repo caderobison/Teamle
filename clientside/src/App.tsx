2c1263601872997a1ae8a5266ab1de836f1a71e3
caderobison <cade.robison@gmail.com>
2025-01-08 23:50:38 -0600

    comment removal

--- clientside/src/App.tsx
+++ clientside/src/App.tsx
@@ -1,28 +1,9 @@
-// import React, { useEffect, useState } from "react";
-// import logo from "./logo.svg";
 import "./App.css";
-import { GuessHandler } from "./Handlers/GuessHandler";
 import { GuessTable } from "./components/GuessScreen/GuessScreen";
 
 function App() {
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.tsx</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       {testString}
-    //     </a>
-    //   </header>
     <GuessTable></GuessTable>
-    // </div>
   );
 }
 
