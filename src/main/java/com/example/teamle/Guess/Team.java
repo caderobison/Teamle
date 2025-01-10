6dbea979435494921d5b9a902894d4c92c0d7cda
caderobison <cade.robison@gmail.com>
2025-01-09 23:34:14 -0600

    refactoring problems again

--- src/main/java/com/example/teamle/Guess/Team.java
+++ src/main/java/com/example/teamle/Guess/Team.java
@@ -1,4 +1,4 @@
-package com.example.teamle.Guess.GuessTypes;
+package com.example.teamle.Guess;
 
 import com.example.teamle.Enums.TeamleEnums.Leagues;
 import com.example.teamle.Enums.TeamleEnums.StatesAndProvinces.StatesAndProvinces;
