a40c83792e92970d6207af61190f185750bea08e
caderobison <cade.robison@gmail.com>
2025-01-10 00:47:10 -0600

    prettier

--- clientside/src/components/GuessScreen/GuessScreen.tsx
+++ clientside/src/components/GuessScreen/GuessScreen.tsx
@@ -1,33 +1,36 @@
-import {Col, Table} from "antd";
-import React, {useEffect, useState} from "react";
-import {LeagueResultCard} from "../ResultCard/ResultCardVariations/LeagueResultCard";
-import {NumberResultCard} from "../ResultCard/ResultCardVariations/NumberResultCard";
-import {StateResultCard} from "../ResultCard/ResultCardVariations/StateResultCard";
-import {GuessResponse, IGuessDataRowProps, TeamSkeleton} from "./GuessScreenTypes";
-import {GuessHandler} from "../../Handlers/GuessHandler";
-import {TeamsHandler} from "../../Handlers/TeamsHandler";
+import { Col, Table } from "antd";
+import React, { useEffect, useState } from "react";
+import { LeagueResultCard } from "../ResultCard/ResultCardVariations/LeagueResultCard";
+import { NumberResultCard } from "../ResultCard/ResultCardVariations/NumberResultCard";
+import { StateResultCard } from "../ResultCard/ResultCardVariations/StateResultCard";
+import {
+  GuessResponse,
+  IGuessDataRowProps,
+  TeamSkeleton,
+} from "./GuessScreenTypes";
+import { GuessHandler } from "../../Handlers/GuessHandler";
+import { TeamsHandler } from "../../Handlers/TeamsHandler";
 
-class GuessTableState{
+class GuessTableState {
   constructor() {
     this.response = null;
-    this.allTeams = null
+    this.allTeams = null;
   }
-  response : GuessResponse | null;
-  allTeams : TeamSkeleton[] | null;
+  response: GuessResponse | null;
+  allTeams: TeamSkeleton[] | null;
 }
 
 export function GuessTable() {
-
-  const[state, setState] = useState<GuessTableState>(new GuessTableState())
+  const [state, setState] = useState<GuessTableState>(new GuessTableState());
   const getData = async () => {
-    console.log("HERE")
+    console.log("HERE");
     const handler = new GuessHandler();
     const resp = await handler.compareGuess(3);
     setState((prevState) => ({
       ...prevState,
       response: resp,
     }));
-  }
+  };
 
   const getAllTeams = async () => {
     const handler = new TeamsHandler();
@@ -36,73 +39,102 @@ export function GuessTable() {
       ...prevState,
       allTeams: resp,
     }));
-  }
+  };
 
   useEffect(() => {
     getData();
     getAllTeams();
-  }, [])
+  }, []);
 
   const columns = [
     {
       title: "League",
       key: "league",
-      render: (record: IGuessDataRowProps) => <LeagueResultCard guess={record.league} answerType={record.leagueAnswerType}/>,
+      render: (record: IGuessDataRowProps) => (
+        <LeagueResultCard
+          guess={record.league}
+          answerType={record.leagueAnswerType}
+        />
+      ),
     },
     {
       title: "Number of Championships",
       key: "championships",
-      render: (record : IGuessDataRowProps) => <NumberResultCard guess={record.numberChampionships} answerType={record.numberChampionshipsAnswerType} answerDirection={record.numberChampionshipsDirection}/>,
+      render: (record: IGuessDataRowProps) => (
+        <NumberResultCard
+          guess={record.numberChampionships}
+          answerType={record.numberChampionshipsAnswerType}
+          answerDirection={record.numberChampionshipsDirection}
+        />
+      ),
     },
     {
       title: "Last Championship",
       key: "lastChampionship",
-      render: (record : IGuessDataRowProps) => <NumberResultCard guess={record.lastChampionship} answerType={record.lastChampionshipAnswerType} answerDirection={record.lastChampionshipDirection}/>,
+      render: (record: IGuessDataRowProps) => (
+        <NumberResultCard
+          guess={record.lastChampionship}
+          answerType={record.lastChampionshipAnswerType}
+          answerDirection={record.lastChampionshipDirection}
+        />
+      ),
     },
     {
       title: "Year founded",
       key: "foundedDate",
-      render: (record : IGuessDataRowProps) => <NumberResultCard guess={record.yearFounded} answerType={record.yearFoundedAnswerType} answerDirection={record.yearFoundedDirection}/>,
+      render: (record: IGuessDataRowProps) => (
+        <NumberResultCard
+          guess={record.yearFounded}
+          answerType={record.yearFoundedAnswerType}
+          answerDirection={record.yearFoundedDirection}
+        />
+      ),
     },
     {
       title: "State or Province",
       key: "state",
-      render: (record : IGuessDataRowProps) => <StateResultCard guess={record.state} answerType={record.stateAnswerType}/>,
+      render: (record: IGuessDataRowProps) => (
+        <StateResultCard
+          guess={record.state}
+          answerType={record.stateAnswerType}
+        />
+      ),
     },
   ];
-if(state.response) {
+  if (state.response) {
+    const dataSource = [
+      {
+        key: "1",
+        league: state.response.league,
+        leagueAnswerType: state.response.leagueAnswerType,
 
-  const dataSource = [
-    {
-      key: "1",
-      league: state.response.league,
-      leagueAnswerType: state.response.leagueAnswerType,
+        state: state.response.state,
+        stateAnswerType: state.response.stateAnswerType,
 
-      state: state.response.state,
-      stateAnswerType: state.response.stateAnswerType,
+        numberChampionships: state.response.numberChampionships,
+        numberChampionshipsAnswerType:
+          state.response.numberChampionshipsAnswerType,
+        numberChampionshipsDirection:
+          state.response.numberChampionshipsDirection,
 
-      numberChampionships: state.response.numberChampionships,
-      numberChampionshipsAnswerType: state.response.numberChampionshipsAnswerType,
-      numberChampionshipsDirection: state.response.numberChampionshipsDirection,
+        lastChampionship: state.response.lastChampionship,
+        lastChampionshipAnswerType: state.response.lastChampionshipAnswerType,
+        lastChampionshipDirection: state.response.lastChampionshipDirection,
 
-      lastChampionship: state.response.lastChampionship,
-      lastChampionshipAnswerType: state.response.lastChampionshipAnswerType,
-      lastChampionshipDirection: state.response.lastChampionshipDirection,
-
-      yearFounded: state.response.yearFounded,
-      yearFoundedAnswerType: state.response.yearFoundedAnswerType,
-      yearFoundedDirection: state.response.yearFoundedDirection,
-    } as IGuessDataRowProps,
-  ];
-  return (
+        yearFounded: state.response.yearFounded,
+        yearFoundedAnswerType: state.response.yearFoundedAnswerType,
+        yearFoundedDirection: state.response.yearFoundedDirection,
+      } as IGuessDataRowProps,
+    ];
+    return (
       <Col span={16} offset={4}>
         <Table
-            dataSource={dataSource}
-            columns={columns}
-            tableLayout="fixed"
-            pagination={false}
+          dataSource={dataSource}
+          columns={columns}
+          tableLayout="fixed"
+          pagination={false}
         />
       </Col>
-  );
-}
+    );
+  }
 }
